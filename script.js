57e1196407be6c19a6e1e7adb10c1b32e709475f
dhamendra sahu <dhmndrsahu@gmailcom>
2024-09-16 13:27:59 +0530

    lendingpage

--- script.js
+++ script.js
@@ -1,52 +1,112 @@
-
-
-
-
-let tl = gsap.timeline();
+function loadingAniation() {
+    let tl = gsap.timeline();
     tl.from(".loading, .loader-img-div img", {
-        y:500,
-        duration:1.3,
-        stagger:0.5,
-        onComplete:()=>{
-
-            let count = 0
-            let counterSpan 
-            let counter = setInterval(()=>{
-                ++count
+        y: 500,
+        duration: 1.3,
+        stagger: 0.5,
+        onComplete: () => {
+            let count = 0;
+            let counter = setInterval(() => {
+                ++count;
                 if (count === 100) {
-                    clearInterval(counter)
+                    clearInterval(counter);
                 }
-                document.querySelector('.counterSpan').textContent = count
+                document.querySelector(".counterSpan").textContent = count;
                 // console.log(count)
-            },20)
-            
-        }
-    })
-    tl.to(".loader",{
-        delay:2,
-        opacity:0,
-        duration :1.3,
-        display:"none"
-    })
-    tl.from(".page1",{
+            }, 20);
+        },
+    });
+    tl.to(".loader", {
+        delay: 2,
+        opacity: 0,
+        duration: 1.3,
+        display: "none",
+    });
+    tl.from(".page1", {
         // delay:-1,
-        duratin:2,
-        y:1600,
-        ease: "power3.out"
-    })
-
-
-    // Shery.imageEffect(".back",{style:6, });
-
-// Shery.imageEffect(".back",{style:6,
-//     config:{"a":{"value":2,"range":[0,30]},"b":{"value":0.75,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":2.082236837977518},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":1},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":15,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":1},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.94,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
-//     gooey:true,
-//     // debug:true
-// })\
+        duratin: 2,
+        y: 1600,
+        ease: "power3.out",
+    });
+   
+}
 
 Shery.mouseFollower({
     //Parameters are optional.
     skew: true,
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
-  });
+});
+Shery.hoverWithMediaCircle(".hero" /* Element to target.*/, {
+    images: ["./assets/it-img-5.webp","./assets/it-img-6.webp"] /*OR*/,
+    // videos: ["./0.mp4", "/1.mp4"],
+  })
+
+let lendingpageHero = [
+    {
+        heading1: "WE EMPOWER YOUR BUSINESS",
+        spanContent:"BUSINESS",
+        heading2: "With Our CUTTING-EDGE IT Solutions",
+        paragraph:
+            "We Design & Develop Custom & Innovative Solutions For Modern Era Business",
+    },
+    {
+        heading1: "WE BUILD POWERFUL",
+        spanContent:"POWERFUL",
+        heading2: "Next Level And Unique Websites.",
+        paragraph:
+            "With More than 15+ years of experience, We offer the best Web development services to give you exactly what you require to achieve your growth targets.",
+    },
+    {
+        heading1: "GROW YOUR BUSINESS",
+        spanContent:"BUSINESS",
+        heading2: "With Our Digital Marketing Service.",
+        paragraph:
+            "We provide customized tailored solutions fast track your online visibility, boost brand Awareness and Increase your sales",
+    },
+];
+
+let initalindex = 0;
+let green = document.querySelector(".green")
+
+let container = document.querySelector(".container");
+let hero =  document.querySelector(".hero")
+let updateContent = (index) => {
+    let content = lendingpageHero[index];
+
+    
+    document.querySelector('.heading-container h1').textContent = content.heading1;
+    document.querySelector('.heading-container h2').textContent = content.heading2;
+    document.querySelector('.heading-container p').textContent = content.paragraph;
+
+    gsap.from(".heading-container h1, .heading-container h2, .heading-container p ", {
+        y:500,
+        stagger:0.5,
+        duration:1,
+        ease:"expo.out"
+        })
+    
+};
+
+container.addEventListener("click", () => {
+    updateContent(initalindex);
+    initalindex = (initalindex + 1) % lendingpageHero.length;
+    let changeContent = setInterval(()=>{
+        updateContent(initalindex);
+        initalindex = (initalindex + 1) % lendingpageHero.length;
+        
+    },10000)
+    changeContent()
+
+});
+
+    
+
+
+
+
+      
+
+loadingAniation();
+
+
